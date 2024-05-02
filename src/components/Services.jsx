afad52a4b98692113675deb2edad4442aaf66558
Tran Cong Ha <toanlyhoa99@gmail.com>
2024-05-02 23:34:14 +0700

    update gif

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import aoDoanImg1 from "../assets/smile.gif";
 
 const Services = () => {
   return (<section className="c-services c-section" id="is-service">
@@ -9,7 +10,13 @@ const Services = () => {
               <span>Thông tin</span> về tôi
          </p>
         <div className="c-service__container l-container">
-              
+              <div className="c-about1__row1">
+                    <div className="c-about1__avatar">
+                         <div className="c-avatar1">
+                              <img src={aoDoanImg1} alt="Thanh Ngoc Tran" />
+                         </div>
+                    </div>
+              </div>
         </div>
   </section>
   )
