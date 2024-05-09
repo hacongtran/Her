8dc87e7f074c56c481cd1a2e1709e56ee9bec622
harry <toanlyhoa99@gmail.com>
2024-05-09 17:02:20 +0700

    9th May Coding

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Home from "./components/Home";
 import Services from "./components/Services";
+import Projects from "./components/Projects";
 import "./App.css";
 import "./styles/mixin.scss";
 import "./styles/reset.scss";
 import "./styles/main.scss";
 import "./styles/home.scss";
 import "./styles/service.scss";
+import "./styles/projects.scss";
 
 function App() {
   return (
     <main className="c-main">
       <Home />
       <Services />
+      <Projects />
     </main>
   );
 }
---
+++ src/Data.jsx
@@ -0,0 +1,28 @@
+import banner1 from './assets/banner_canva1.png';
+import banner2 from './assets/banner_canva2.jpg';
+import banner3 from './assets/banner_canva3.png';
+import banner4 from './assets/banner_canva4.png';
+
+export const projects = [
+    {
+        id: 1,
+        img: banner1,
+        alt: 'Golden Peony - Tinh chất trẻ hóa làn da',
+    },
+    {
+        id: 2,
+        img: banner2,
+        alt: 'Houmal - Tinh chất hút tàn nhang, đồi mồi',
+    },
+    {
+        id: 3,
+        img: banner3,
+        alt: 'Anti - acne Kem trị sẹo mờ thâm',
+    },
+    {
+        id: 4,
+        img: banner4,
+        alt: 'Syrinx - sữa sử mặt nội địa trung',
+    }
+    
+]
