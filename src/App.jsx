93a02dfa81ec8bdb814a110c52217258f01dbd1c
harry <toanlyhoa99@gmail.com>
2024-05-23 16:45:55 +0700

    23rd May 2024

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./components/Home";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import "./App.css";
 import "./styles/mixin.scss";
 import "./styles/reset.scss";
@@ -11,14 +13,18 @@ import "./styles/home.scss";
 import "./styles/service.scss";
 import "./styles/projects.scss";
 import "./styles/contact.scss";
+import "./styles/footer.scss";
+import "./styles/header.scss";
 
 function App() {
   return (
     <main className="c-main">
+      <Header />
       <Home />
       <Services />
       <Projects />
       <Contact />
+      <Footer />
     </main>
   );
 }
